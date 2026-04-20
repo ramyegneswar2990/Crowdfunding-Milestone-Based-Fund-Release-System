43dd4a093984df63a96049378e02dad59e9469a3
ramyegneswar2990 <ramyegneswarseeramm@gmail.com>
2026-04-20 11:24:23 +0530

    first commit

---
+++ Backend/src/test/java/com/example/CrowdFunding/CrowdFundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrowdFunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdFundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
